ffd5e38d788c5d3f5b160c86d24bb428fc1e725c
gabriel gomes <gabrielluizgomes1@gmail.com>
2023-08-09 22:09:19 -0300

    feat: add swagger doc

--- libs/domain/src/users/dto/create-user.dto.ts
+++ libs/domain/src/users/dto/create-user.dto.ts
@@ -1,6 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
 export class CreateUserDto {
+    //@ApiProperty()
     id: string;
+    @ApiProperty({
+        description: 'Nome do usuário',
+        example: 'João da Silva',
+        type: String,
+    })
     name: string;
+    @ApiProperty({
+        description: 'Email do usuário',
+        example: 'exemplo@gmail.com',
+        type: String,
+    })
     email: string;
+    @ApiProperty({
+        description: 'Senha do usuário',
+        example: '123456',
+        type: String,
+    })
     password: string;
 }
